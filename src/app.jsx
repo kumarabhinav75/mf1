82216f1d10e209d08a1ed32cf7ac1c2a280ec808
abhinavkumar75 <abhinav.kumar@cleartax.in>
2024-01-22 03:05:57 +0530

    initial setup mf1

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Style from './style.css';
+
+const App = () => {
+
+  return (
+    <div className={'mf1'}>
+      <h1>MF1 Application</h1>
+    </div>
+  );
+};
+
+export default App;
